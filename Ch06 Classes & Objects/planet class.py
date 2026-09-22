ef591d1a097c5851a9abdb6ce259fedbb8ff8646
EsclovrRivulet <esclovr@gmail.com>
2026-09-22 22:10:38 +0800

    Add classes, OOP, and linear data structure exercises

---
+++ Ch06 Classes & Objects/planet class.py
@@ -0,0 +1,28 @@
+class Planet:
+    def __init__(self, name, planet_type, star):
+        if not all(isinstance(arg, str) for arg in (name, planet_type, star)):
+            raise TypeError('name, planet type, and star must be strings')
+        elif any(not arg for arg in (name, planet_type, star)):
+            raise ValueError('name, planet_type, and star must be non-empty strings')
+        
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+
+    def orbit(self):
+        return f'{self.name} is orbiting around {self.star}...'
+
+    def __str__(self):
+        return f'Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}'
+
+planet_1 = Planet('Mercury', 'Terrestrial', 'Sun')
+planet_2 = Planet('Jupiter', 'Gas giant', 'Sun')
+planet_3 = Planet('Proxima Centauri b', 'Terrestrial', 'Proxima Centauri')
+
+print(planet_1)
+print(planet_2)
+print(planet_3)
+
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
